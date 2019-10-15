22e7db73e96cc6f1e4494757acb8fd74ee287377
A.B. Carroll <ben@hl9.net>
2019-10-15 04:21:38 -0400

    Simple tests.

--- tests/MimeTypeArrayTest.php
+++ tests/MimeTypeArrayTest.php
@@ -1,30 +1,29 @@
 <?php
-/**
- * @file    MimeTypeArrayTest.php
- * @package {lib-mime} IntelliJ IDEA
- * @package ab\Mime
- * @author  A.B. Carroll <ben@hl9.net>
- */
 
 namespace ab\Mime;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * MimeTypeArrayTest
+ *
+ * @package ab\Mime
+ * @author  A.B. Carroll <ben@hl9.net>
+ */
 class MimeTypeArrayTest extends TestCase
 {
 
     public function testGetContentType()
     {
-
-    }
-
-    public function testGetFileExt()
-    {
-
-    }
-
-    public function testGetTypeToExtArray()
-    {
-
+        $this->assertEquals('text/html', MimeTypeArray::getContentType('html'));
+        $this->assertEquals('text/html', MimeTypeArray::getContentType('htm'));
+        $this->assertEquals('text/html', MimeTypeArray::getContentType('.html'));
+        $this->assertEquals('text/html', MimeTypeArray::getContentType('.htm'));
+        $this->assertEquals('text/html', MimeTypeArray::getContentType('.HTML'));
+        $this->assertEquals('text/html', MimeTypeArray::getContentType('.htM'));
+        $this->assertEquals('text/css', MimeTypeArray::getContentType('.CSS'));
+        $this->assertEquals('text/csv', MimeTypeArray::getContentType('.csv'));
+        $this->assertEquals('application/javascript', MimeTypeArray::getContentType('js'));
+        $this->assertEquals('application/javascript', MimeTypeArray::getContentType('.JS'));
     }
 }
